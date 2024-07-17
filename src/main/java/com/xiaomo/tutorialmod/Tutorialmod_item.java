1f07e33dbaa8410178a3b8d586dac1b34fe1e07f
XiaoMo <xiaomo3250@qq.com>
2024-07-17 17:03:26 +0800

    item完成

--- src/main/java/com/xiaomo/tutorialmod/Tutorialmod_item.java
+++ src/main/java/com/xiaomo/tutorialmod/Tutorialmod_item.java
@@ -9,7 +9,7 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Tutorialmod implements ModInitializer {
+public class Tutorialmod_item implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
